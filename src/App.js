f6c979d309b0782b86b07dccde553cbad24021b3
fukupee <fukupeemail@gmail.com>
2021-11-14 15:04:20 +0900

    Dashbord とりあえず起動確認

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 import Button from '@material-ui/core/Button';
-
+import Dashboard from './dashboard/Dashboard';
 
 function App() {
   return (
     <Button variant="contained" color="primary">
       Hello World
     </Button>
-    /*
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    */   
   );
 }
 
